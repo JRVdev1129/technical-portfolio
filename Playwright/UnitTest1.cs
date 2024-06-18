d89cfb4821680ab30170cbebcb05416ee28ab3a3
JakocR <sam_rivas_sime@outlook.com>
2024-06-18 16:35:18 -0400

    refactor: moves get started and instalattion buttons locators to a new var

--- Playwright/UnitTest1.cs
+++ Playwright/UnitTest1.cs
@@ -22,11 +22,14 @@ public class ExampleTest : PageTest
     public async Task GetStartedLink()
     {
         await Page.GotoAsync("https://playwright.dev");
+        var getStartedLink = Page.GetByRole(AriaRole.Link, new() { Name = "Get started" });
 
         // Click the get started link.
-        await Page.GetByRole(AriaRole.Link, new() { Name = "Get started" }).ClickAsync();
+        await getStartedLink.ClickAsync();
+
+        var installationButton = Page.GetByRole(AriaRole.Heading, new() { Name = "Installation" });
 
         // Expects page to have a heading with the name of Installation.
-        await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Installation" })).ToBeVisibleAsync();
+        await Expect(installationButton).ToBeVisibleAsync();
     }
 }
