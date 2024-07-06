92c22b59c76c1dcf079d9ed5064a20b348769196
jr <sam_rivas_sime@outlook.com>
2024-07-06 15:17:51 -0400

    feat: creates new directory for the heroku internet page

--- WebdriverioTypescript/test/specs/internet/test.e2e.ts
+++ WebdriverioTypescript/test/specs/internet/test.e2e.ts
@@ -1,6 +1,6 @@
 import { expect } from '@wdio/globals'
-import LoginPage from '../../page-objects/login.page.js'
-import SecurePage from '../../page-objects/secure.page.js'
+import LoginPage from '../../../page-objects/login.page.js'
+import SecurePage from '../../../page-objects/secure.page.js'
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
