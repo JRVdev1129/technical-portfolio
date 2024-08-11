72f1d8d551542e940f081ad00f8e9d2f8c4479f5
jr <sam_rivas_sime@outlook.com>
2024-08-11 00:30:08 -0400

    feat: creates initial header unit test

--- JestJavascript/tests/spec/componentTesting/first-example-project/Header/header.test.js
+++ JestJavascript/tests/spec/componentTesting/first-example-project/Header/header.test.js
@@ -1,13 +1,13 @@
 import { cleanup, fireEvent, render, screen } from "@testing-library/react";
-import Header from "../../../../react-example-project/src/components/Header/Header.js";
+import Header from "../../../../../react-example-project/src/components/Header/Header.js";
 
 // Note: running cleanup afterEach is done automatically for you in @testing-library/react@9.0.0 or higher
 // unmount and cleanup DOM after the test is finished.
 afterEach(cleanup);
 
 it("CheckboxWithLabel changes the text after click", () => {
-  render(<Header header="title" />);
-  const headerElement = screen.getByTestId("header");
+  render(<Header title="header" />);
+  const headerElement = screen.getByText("header");
 
   expect(headerElement).toBeInTheDocument();
 
