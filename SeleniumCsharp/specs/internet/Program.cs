10592bd59c35792f4ea1b3a41bd600ca382d36ae
jr <sam_rivas_sime@outlook.com>
2024-12-17 09:01:56 -0500

    feat: adds basic configuration, and test specs plus page objects to see that everything is working as expected before starting to write the actual tests

--- SeleniumCsharp/specs/internet/Program.cs
+++ SeleniumCsharp/specs/internet/Program.cs
@@ -1,52 +1,144 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CSharpSelFramework.pageObjects;
+using CSharpSelFramework.utilities;
+using NUnit.Framework;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Chrome;
-using SeleniumCSharp.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using WebDriverManager.DriverConfigs.Impl;
 
-namespace SeleniumCSharp
+namespace SeleniumLearning
 {
-    class Program
+    [Parallelizable(ParallelScope.Self)]
+    public class E2ETest : Base
     {
-        static void Main(string[] args)
+
+        [Test, TestCaseSource("AddTestDataConfig"), Category("Regression")]
+        //[TestCase("rahulshettyacademy", "learning")]
+        //[TestCase("rahulshetty", "learning")]
+
+        // run all data sets of Test method in parallel  - Done
+        // run all test methods in one class parallel   - Done
+        // run all test files in project parallel   - Done
+
+        //dotnet test pathto.csproj ( ALl tests)
+        //dotnet test pathto.csproj --filter TestCategory=Smoke
+
+        // dotnet test CSharpSelFramework.csproj --filter TestCategory=Smoke -- TestRunParameters.Parameter\(name=\"browserName\",value=\"Chrome\"\)
+
+        [Parallelizable(ParallelScope.All)]
+        public void EndToEndFlow(String username, String password, String[] expectedProducts)
+
         {
-            // Set Chrome options to run headless with necessary flags for Docker
-            var options = new ChromeOptions();
-            options.AddArgument("--headless"); // Run Chrome in headless mode
-            options.AddArgument("--no-sandbox"); // Bypass OS security model
-            options.AddArgument("--disable-dev-shm-usage"); // Overcome limited resource problems
-            options.AddArgument("--disable-gpu"); // Applicable to Docker environment, even if GPU is not available
-            options.AddArgument("--window-size=1920,1080");
-            // Set screen size (optional)
-            // Bind to 0.0.0.0 for Docker compatibility
-            // options.AddArgument("--remote-debugging-address=0.0.0.0");
-            options.AddArgument("--remote-debugging-port=9222");
-            options.AddArgument("--ignore-ssl-errors=yes");
-            options.AddArgument("--ignore-certificate-errors");
-            options.AddArgument("--log-level=ALL"); // Set Chrome's log level to all
-            options.AddArgument("--verbose"); // Enable verbose logging
+            //username1 -100
+            //Login page ->
+            //Documents request ->
+            //
+
+           // String[] expectedProducts = { "iphone X", "Blackberry" };
+            String[] actualProducts = new string[2];
+            LoginPage loginPage = new LoginPage(getDriver());
+            ProductsPage productPage =loginPage.validLogin(username,password);
+            productPage.waitForPageDisplay();
+
+            IList<IWebElement> products = productPage.getCards();
+
+            foreach (IWebElement product in products)
+            {
+
+                if (expectedProducts.Contains(product.FindElement(productPage.getCardTitle()).Text))
+
+                {
+                    product.FindElement(productPage.addToCartButton()).Click();
+                }
+
+            }
+            CheckoutPage checkoutPage =productPage.checkout();
+
+            IList<IWebElement> checkoutCards = checkoutPage.getCards();
+
+            for (int i = 0; i < checkoutCards.Count; i++)
+
+            {
+                actualProducts[i] = checkoutCards[i].Text;
 
 
 
-            // Initialize the ChromeDriver
-            using IWebDriver driver = new ChromeDriver(options);
+            }
+            Assert.That(expectedProducts,Is.EqualTo(actualProducts));
 
-            // Navigate to Google
-            driver.Navigate().GoToUrl("https://www.google.com");
-            Console.WriteLine("Page Title: " + driver.Title);
+            checkoutPage.checkOut();
 
 
-            // Initialize the GoogleHomePage object
-            var googleHomePage = new GoogleHomePage(driver);
 
-            // Perform a search using the page object
-            googleHomePage.Search("Selenium WebDriver");
 
-            // Close the browser after some delay
-            System.Threading.Thread.Sleep(5000);
-            driver.Quit();
+            driver.Value.FindElement(By.Id("country")).SendKeys("ind");
+            WebDriverWait wait = new WebDriverWait(driver.Value, TimeSpan.FromSeconds(8));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.LinkText("India")));
+            driver.Value.FindElement(By.LinkText("India")).Click();
+
+
+            driver.Value.FindElement(By.CssSelector("label[for*='checkbox2']")).Click();
+            driver.Value.FindElement(By.CssSelector("[value='Purchase']")).Click();
+            String confirText = driver.Value.FindElement(By.CssSelector(".alert-success")).Text;
+
+            // StringAssert.Contains("Success", confirText);
+            Assert.That(confirText, Does.Contain("Success"));
+
+
+
+
+        }
+
+
+
+        [Test,Category("Smoke")]
+        public void LocatorsIdentification()
+
+        {
+
+            driver.Value.FindElement(By.Id("username")).SendKeys("rahulshettyacademy");
+            driver.Value.FindElement(By.Id("username")).Clear();
+            driver.Value.FindElement(By.Id("username")).SendKeys("rahulshetty");
+            driver.Value.FindElement(By.Name("password")).SendKeys("123456");
+            //css selector & xpath
+            //  tagname[attribute ='value']
+            //    #id  #terms  - class name -> css .classname
+            //    driver.FindElement(By.CssSelector("input[value='Sign In']")).Click();
+
+            //    //tagName[@attribute = 'value']
+
+            // CSS - .text-info span:nth-child(1) input
+            //xpath - //label[@class='text-info']/span/input
+
+            driver.Value.FindElement(By.XPath("//div[@class='form-group'][5]/label/span/input")).Click();
+
+            driver.Value.FindElement(By.XPath("//input[@value='Sign In']")).Click();
+
+            WebDriverWait wait = new WebDriverWait(driver.Value, TimeSpan.FromSeconds(8));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions
+           .TextToBePresentInElementValue(driver.Value.FindElement(By.Id("signInBtn")), "Sign In"));
+
+            String errorMessage = driver.Value.FindElement(By.ClassName("alert-danger")).Text;
+            TestContext.Progress.WriteLine(errorMessage);
+
+
+        }
+
+
+
+
+        public static IEnumerable<TestCaseData> AddTestDataConfig()
+
+        {
+
+          yield return new TestCaseData(getDataParser().extractData("username"), getDataParser().extractData("password"), getDataParser().extractDataArray("products"));
+            yield return new TestCaseData(getDataParser().extractData("username"), getDataParser().extractData("password"), getDataParser().extractDataArray("products"));
+            yield return new TestCaseData(getDataParser().extractData("username_wrong"), getDataParser().extractData("password_wrong"), getDataParser().extractDataArray("products"));
         }
     }
-}
 
+}
 
